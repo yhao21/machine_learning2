b297c3903e30fea5846ba2519fe9c4d9a1ea8f83
yhao21 <13701304462@163.com>
2021-03-11 12:18:14 -0500

    gmm_pic

--- 861/pic_verification/pic_analysis.py
+++ 861/pic_verification/pic_analysis.py
@@ -3,6 +3,7 @@ import imageio, glob, os, re
 import numpy as np
 from sklearn.mixture import GaussianMixture as gmm
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
 
 
@@ -37,6 +38,7 @@ if __name__ == '__main__':
     # go through each pictures
     # one_file stands for the file path
     df_list = []
+    ## notice, glob may not go through with order, i.e., pic1, pic2,.. it will be pic3, pic5, pic1, ...
     for one_file in glob.glob(os.path.join(os.getcwd(), 'data', '*.jpeg')):
         image_one = get_rgb(one_file).tolist()
         file_name = re.compile(r'data/(.*?)\.jpeg').findall(one_file)[0]
@@ -47,28 +49,44 @@ if __name__ == '__main__':
         
 
     df = pd.DataFrame(df_list, columns = ['R', 'G', 'B', 'name'])
-    print(df)
+    #print(df)
 
 
     x1 = df.iloc[:, 0].values
     x2 = df.iloc[:, 1].values
     x3 = df.iloc[:, 2].values
-    plt.plot(x1, x2, '.')
-    plt.savefig('color_x1_x2.png')
-    plt.clf()
 
-    plt.plot(x1, x3, '.')
-    plt.savefig('color_x1_x3.png')
-    plt.clf()
+    ### plotting
+    #plt.plot(x1, x2, '.')
+    #plt.savefig('color_x1_x2.png')
+    #plt.clf()
 
-
-    plt.plot(x2, x3, '.')
-    plt.savefig('color_x2_x3.png')
-    plt.clf()
+    #plt.plot(x1, x3, '.')
+    #plt.savefig('color_x1_x3.png')
+    #plt.clf()
 
 
+    #plt.plot(x2, x3, '.')
+    #plt.savefig('color_x2_x3.png')
+    #plt.clf()
+
+    gmm_data = df.iloc[:, 0:3].values
+    ## use sklearn method to normalize
+    gmm_data = preprocessing.normalize(gmm_data)
+    machine = gmm(n_components=4)
+    machine.fit(gmm_data)
+    gmm_result = machine.predict(gmm_data)
+    print(gmm_result)
+
+    plt.scatter(x1,x2,c = gmm_result)
+    plt.savefig('gmm_result.png')
+    df['gmm_result'] = gmm_result
+    print(df)
 
 
+    #sort the value
+    df = df.sort_values(by=['name'])
+    print(df)
 
 
 
